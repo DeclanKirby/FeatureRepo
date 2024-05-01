1668ada207e97e8c72012827c5f65635145b800b
DeclanKirby <declanpkirby@gmail.com>
2024-04-30 19:12:09 -0700

    Player Movement
    player can move using WASD

--- DKIRBY_Feature/Assets/Scripts/PlayerController.cs
+++ DKIRBY_Feature/Assets/Scripts/PlayerController.cs
@@ -1,12 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public GameObject playerReference;
+
+    
+
+    public float sensitivity = 2.0f;
+
+    public float speed = 4f;
+
+    private CharacterController characterController;
+
+    private Vector3 direction;
+
+    private Vector2 _input;
     private void Awake()
     {
-        playerReference = GameObject.Find("PlayerContainer");
+        characterController = GetComponent<CharacterController>();
+        //playerReference = GameObject.Find("PlayerContainer");
+    }
+    private void Update()
+    {
+
+        characterController.Move(direction * speed * Time.deltaTime);
+        
+    }
+
+    
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        
+        _input = context.ReadValue<Vector2>();
+        direction = new Vector3(_input.x, 0f, _input.y);
     }
 }
---
+++ DKIRBY_Feature/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+
+    [SerializeField]
+    float minViewDistance = 1f;
+
+    public float mouseSensitivity = 100f;
+
+    [SerializeField]
+    Transform playerBody;
+
+    float xRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+}
